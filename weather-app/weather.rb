8b6ec5e445a68de8b1fc208c16609cd208f61743
Angela Stanford <stanfordal7@gmail.com>
2015-02-06 09:02:58 -0500

    Update

--- weather-app/weather.rb
+++ weather-app/weather.rb
@@ -1,5 +1,10 @@
 require 'weather-underground'
 
 puts "Please enter your location"
-get_weather = gets
-s
+location = gets
+
+def weather (location)
+    client = Weatherman::Client.new
+    client.lookup_by_location{@location}.condition['temp']
+    puts "The weather today will be #{temp}"
+    end
