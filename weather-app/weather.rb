3c571908201f5298da8d252b2f45025880b04fe9
Angela Stanford <stanfordal7@gmail.com>
2015-02-08 13:28:19 -0500

    Weather App First edition

--- weather-app/weather.rb
+++ weather-app/weather.rb
@@ -1,10 +1,43 @@
-require 'weather-underground'
+require 'sinatra'
+require 'yahoo_weatherman'
 
-puts "Please enter your location"
-location = gets
+def get_weather(location)
+client = Weatherman::Client.new
+client.lookup_by_location(location).condition['text']
+end
 
-def weather (location)
-    client = Weatherman::Client.new
-    client.lookup_by_location{@location}.condition['temp']
-    puts "The weather today will be #{temp}"
+def get_temp(location)
+client = Weatherman::Client.new
+client.lookup_by_location(location).condition['temp']
+end
+
+get '/' do
+erb :home
+end
+
+post '/weather' do
+    @post = params[:post]['location']
+    @weather = get_weather(@post).downcase
+    @temp = ((get_temp(@post) * 9)/5) + 32
+
+   " #{@weather}"
+    if(@weather == 'sunny')
+        erb :sunny
+    elsif(@weather == 'fair')
+        erb :sunny
+    elsif (@weather == 'cloudy')
+        erb :cloudy
+    elsif (@weather == 'mostly cloudy')
+        erb :cloudy
+    elsif (@weather == 'partly cloudy')
+        erb :cloudy
+    elsif (@weather == 'rain')
+        erb :rainy
+    elsif (@weather == 'light rain with thunder')
+        erb :rainy
+    elsif (@weather == 'snowy')
+        erb :snowy
+    else
+        erb :default
     end
+end
